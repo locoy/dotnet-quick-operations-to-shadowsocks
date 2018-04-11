f179ee8b9867b9703a20459574fbc9fafc5f1a75
Phi <optimusgrass@live.com>
2018-04-11 22:06:09 +0800

    ...

---
+++ QuickOps/QuickOps/TrayIcon.cs
@@ -0,0 +1,37 @@
+﻿using QuickOps.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuickOps
+{
+    class TrayIcon : ApplicationContext
+    {
+        private NotifyIcon trayIcon;
+        //private ClipboardCleaner MyClipboardCleaner = new ClipboardCleaner();
+
+        public TrayIcon()
+        {
+            // Initialize Tray Icon
+            trayIcon = new NotifyIcon()
+            {
+                //Icon = Resources.AppTrayIcon,
+                Icon = Resources.Plane,
+                ContextMenu = new ContextMenu(new MenuItem[] { new MenuItem("Exit", Exit) }),
+                Visible = true,
+                Text = "Click to show configure page."
+            };
+        }
+
+        void Exit(object sender, EventArgs e)
+        {
+            // Hide tray icon, otherwise it will remain shown until user mouses over it
+            trayIcon.Visible = false;
+            Application.Exit();
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # dotnet-quick-operations-to-shadowsocks
-Add some quick operations to shadowsocks.
+
+## Add some quick operations to shadowsocks
+
+* Left click the trayicon to show configure page.
+* Unsuccessfull http requsts will be shown in the configure page.
+* Click "add to list" buttom to add the url to configure.json.
