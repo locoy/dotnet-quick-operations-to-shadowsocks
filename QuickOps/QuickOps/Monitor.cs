4d4ee678049ad90d9326611a07f2e27c36ffd2ec
Phi <optimusgrass@live.com>
2018-04-12 23:25:03 +0800

    ...

---
+++ QuickOps/QuickOps/Monitor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace QuickOps
+{
+    class Monitor
+    {
+        public Stream output;
+
+        public void CaptureHttp()
+        {
+            using (HttpListener listener = new HttpListener())
+            {
+                listener.Prefixes.Add("http://*:80");
+                listener.Prefixes.Add("https://*:80");
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerRequest request = context.Request;
+                // Obtain a response object.
+                HttpListenerResponse response = context.Response;
+                // Construct a response.
+                string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                // Get a response stream and write the response to it.
+                response.ContentLength64 = buffer.Length;
+                output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+                
+                // You must close the output stream.
+                //output.Close();
+                //listener.Stop();
+            }
+        }
+    }
+}
--- QuickOps/QuickOps/TrayIcon.cs
+++ QuickOps/QuickOps/TrayIcon.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,6 +13,9 @@ namespace QuickOps
     {
         private NotifyIcon trayIcon;
         //private ClipboardCleaner MyClipboardCleaner = new ClipboardCleaner();
+        private QuickOpsForm quickOpsForm;
+        private Monitor monitor;
+        private Thread writeThread;
 
         public TrayIcon()
         {
@@ -22,8 +26,13 @@ namespace QuickOps
                 Icon = Resources.Plane,
                 ContextMenu = new ContextMenu(new MenuItem[] { new MenuItem("Exit", Exit) }),
                 Visible = true,
-                Text = "Click to show configure page."
+                Text = "Click to show quick ops form."
             };
+            quickOpsForm = new QuickOpsForm();
+            monitor = new Monitor();
+            monitor.CaptureHttp();
+            quickOpsForm.Visible = false;
+            trayIcon.MouseClick += new MouseEventHandler(ShowQuickOpsForm);
         }
 
         void Exit(object sender, EventArgs e)
@@ -33,5 +42,21 @@ namespace QuickOps
             Application.Exit();
         }
 
+        void ShowQuickOpsForm(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (quickOpsForm.Visible == false)
+                {
+                    quickOpsForm.Visible = true;
+                }
+                else
+                {
+                    quickOpsForm.Visible = false;
+                }
+            }
+        }
+
+        //when the form is visible, stick it in the front and refresh data every 2 seconds;
     }
 }
